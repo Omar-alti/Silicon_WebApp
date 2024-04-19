f520665ef76f84373d9d9a6b4125823a61f76425
Omar-alti <omar.altinawi@utb.ecutbildning.se>
2024-04-19 17:23:13 +0200

    Add project files.

---
+++ Infrastructure/Entities/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
+
+public class UserEntity : IdentityUser
+{
+    [Key]
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? ProfileImage { get; set; } = "avatar.jpg";
+    public string? Bio { get; set; }
+    public int? AddressId { get; set; }
+    public AddressEntity? Address { get; set; }
+
+}
